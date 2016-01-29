e5dc3e68920bc458391db72bc06f699b3c4b1c3d
Jason Hills <jasonahills@gmail.com>
2016-01-29 13:36:47 -0700

    removes some files that accidentally got into the repo

--- src/settings.js
+++
@@ -1,4 +0,0 @@
-var path = require('path');
-exports.TEMP_DIRECTORY = path.join(__dirname, '../temp');
-exports.TEMPLATE_DIRECTORY = path.join(__dirname, '../src/template');
-exports.TEMPLATE_PATH = path.join(exports.TEMPLATE_DIRECTORY, 'report.html');
